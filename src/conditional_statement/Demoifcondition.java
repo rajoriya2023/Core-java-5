51064adbb3eb953568e367d3383b884f4c7b9582
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-04-05 19:45:18 +0530

    3rd apirl assignment

--- src/conditional_statement/Demoifcondition.java
+++ src/conditional_statement/Demoifcondition.java
@@ -1,4 +1,52 @@
 package conditional_statement;
 
 public class Demoifcondition {
+    public static void main(String[]args){
+
+        int age;
+        age=70;
+        if(age>=70){
+            System.out.println("person is eligible for voting " +age);
+       age=18;
+       if (age>=18){
+          System.out.println("is eligible for voting " +age);
+
+          age=17;
+          if(age<=18){
+              System.out.println(" is not eligible for voting " +age);
+
+              age=7;
+              if(age>=6){
+                  System.out.println("is not get addmission");
+
+                  String city="Agra";
+                  if(city =="Agra"){
+                      System.out.println("person belongs to agra");
+
+                      city="Delhi";
+                      if(city == "Delhi"){
+                          System.out.println("person belongs to Delhi");
+
+                          city="Puneb";
+                          if(city!="Tundla"){
+                              System.out.println("person does not belongs to Tundla");
+                          }
+                      }
+                  }
+
+
+              }
+
+          }
+
+       }
+        }
+
+
+
+    }
+
+
+
+
 }
---
+++ src/conditional_statement/Demoifelsecondition.java
@@ -0,0 +1,60 @@
+package conditional_statement;
+
+public class Demoifelsecondition{
+
+    public static void main(String[]args) {
+
+        String city = "Agra";
+
+        if( city == "Agra") {
+            System.out.println("person belongs to Agra");
+
+        } else {
+            System.out.println("preson does not belongs to Agra");
+
+        }
+        city="pune";
+        if(city== "Delhi") {
+            System.out.println("person belongs to pune");
+
+        } else{
+            System.out.println("person does not belongs to pune");
+        }
+        city="Firozabad";
+           if(city=="Firozabad"){
+               System.out.println("person belongs to Firozabad ");
+           } else {
+               System.out.println("person does not belongs to Firozabad");
+           }
+           int age;
+
+           int age1 = 18;
+
+           if (age1 >=12){
+               System.out.println("person eligible for voting");
+
+           } else{
+               System.out.println("person does not eligible for voting");
+           }
+           int age2 = 18;
+           if(age2<=16){
+               System.out.println("person eligible for voting");
+           } else {
+               System.out.println("person not eligible for voting");
+           }
+
+
+
+
+
+
+
+
+
+
+
+    }
+            }
+
+
+
